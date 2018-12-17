41b02274f47a73d845f151b7e5b980b83d7a4f6e
German <germanovichpavel95@gmail.com>
2018-12-17 23:11:54 +0300

    commit

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -1,39 +1,33 @@
 package com.javarush.task.task20.task2003;
-
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
-/* 
-Знакомство с properties
-*/
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() throws IOException {
+    public void fillInPropertiesMap() throws Exception {
         //implement this method - реализуйте этот метод
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = reader.readLine();
-        FileInputStream fileInputStream = new FileInputStream(fileName);
-        try {
-            load(fileInputStream);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+        load(new FileInputStream(new BufferedReader(new InputStreamReader(System.in)).readLine()));
     }
 
     public void save(OutputStream outputStream) throws Exception {
         //implement this method - реализуйте этот метод
-        outputStream.write(properties);
+        Properties p = new Properties();
+        properties.forEach((k, v) -> p.setProperty(k, v));
+        p.store(outputStream, null);
+        outputStream.close();
     }
 
     public void load(InputStream inputStream) throws Exception {
         //implement this method - реализуйте этот метод
-
+        Properties p = new Properties();
+        p.load(inputStream);
+        p.forEach((k, v) -> properties.put(String.valueOf(k), String.valueOf(v)));
+        inputStream.close();
     }
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws Exception {
     }
 }
